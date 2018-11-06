a0b0d8e42f965e73ee21ce3bc5165dbce95dbae5
Roman <arct1k77@gmail.com>
2018-11-06 18:18:41 +0400

    Initial commit

---
+++ core/src/main/java/com/wtr/core/presentation/ui/BaseActivity.kt
@@ -0,0 +1,13 @@
+package com.wtr.core.presentation.ui
+
+import android.os.Bundle
+import com.arellomobile.mvp.MvpAppCompatActivity
+
+abstract class BaseActivity : MvpAppCompatActivity() {
+    abstract var layoutResId: Int
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(layoutResId)
+    }
+}
